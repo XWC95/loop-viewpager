c343c69fc3eae50aabff1cd01b5f07c2e04fbd88
xwc <m13052509803@163.com>
2018-04-06 14:02:16 +0800

    travis

--- banner/src/main/java/com/xwc/view/Banner.java
+++ banner/src/main/java/com/xwc/view/Banner.java
@@ -16,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 
 import java.util.ArrayList;
