bc10be1f392cc516112c8698d9d6d189dfd435db
xwc520 <m13052509803@163.com>
2018-04-08 10:27:48 +0800

    -

--- banner/src/main/java/com/xwc/view/ImageLoader.java
+++ banner/src/main/java/com/xwc/view/ImageLoader.java
@@ -5,6 +5,7 @@ import android.widget.ImageView;
 
 public abstract class ImageLoader implements ImageLoaderInterface<ImageView> {
 
+
     @Override
     public ImageView createImageView(Context context) {
         ImageView imageView = new ImageView(context);
